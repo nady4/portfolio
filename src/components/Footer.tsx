50a2227882bbbe4c35109b0a1a27ff099a30227f
nady4 <dev@nady4.com>
2026-02-22 22:27:59 -0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,8 @@ import { useTranslations } from "~/routes/layout";
 import "../styles/Footer.scss";
 import GithubLogo from "../assets/github.svg";
 import LinkedInLogo from "../assets/linkedIn.svg";
+import XLogo from "../assets/x.svg";
+import InstagramLogo from "../assets/ig.svg";
 
 export default component$(() => {
   const t = useTranslations().value;
@@ -11,12 +13,20 @@ export default component$(() => {
     <footer class="footer">
       <p class="footer-text">{t.footer_name}</p>
       <p class="footer-text">{t.footer_mail}</p>
-      <a href="https://github.com/nady4" target="_blank">
-        <img src={GithubLogo} alt="Github" width="32" height="32" />
-      </a>
-      <a href="https://www.linkedin.com/in/nady4" target="_blank">
-        <img src={LinkedInLogo} alt="LinkedIn" width="32" height="32" />
-      </a>
+      <div class="footer-socials">
+        <a href="https://github.com/nady4" target="_blank">
+          <img src={GithubLogo} alt="Github" width="32" height="32" />
+        </a>
+        <a href="https://www.linkedin.com/in/nady4" target="_blank">
+          <img src={LinkedInLogo} alt="LinkedIn" width="32" height="32" />
+        </a>
+        <a href="https://x.com/_nady4" target="_blank">
+          <img src={XLogo} alt="X" width="32" height="32" />
+        </a>
+        <a href="https://www.instagram.com/nady4_dev" target="_blank">
+          <img src={InstagramLogo} alt="Instagram" width="32" height="32" />
+        </a>
+      </div>
     </footer>
   );
 });
